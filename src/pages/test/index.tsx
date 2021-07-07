700a3ab97108d8f670e040ab0f4e199292350a5d
Michael Belov <mikle0706@icloud.com>
2021-07-07 10:01:38 +0300

    query pagination test

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,27 +1,42 @@
-import React from "react";
-import { useQuery } from "react-query";
-import { Button, DatePicker } from "antd";
+import React from 'react';
+import { useQuery } from 'react-query';
+import { Button, DatePicker } from 'antd';
 
 const TestPage = () => {
-  const { isLoading, data } = useQuery("jsonplaceholder", () =>
-    fetch("https://jsonplaceholder.typicode.com/posts/1/comments").then((res) =>
-      res.json()
-    )
+  const [state, setstate] = React.useState(1);
+
+  const { isLoading, data } = useQuery(['jsonplaceholder', state], () =>
+    fetch(`https://jsonplaceholder.typicode.com/posts/${state}/comments`).then((res) => res.json())
   );
 
   return (
     <>
+      <button
+        type="button"
+        onClick={() => {
+          setstate((prev) => prev + 1);
+        }}>
+        +
+      </button>
+      <button
+        type="button"
+        onClick={() => {
+          setstate((prev) => prev - 1);
+        }}>
+        -
+      </button>
       <div>
         <Button type="primary">PRESS ME</Button>
         <DatePicker placeholder="select date" />
       </div>
       {isLoading && <h1>Loading...</h1>}
-      {data?.map((el: any) => (
-        <React.Fragment key={el.id}>
-          <span>{`EMAIL: ${el.email}`}</span>
-          <p>{`BODY: ${el.body}`}</p>
-        </React.Fragment>
-      ))}
+      {!isLoading &&
+        data?.map((el: any) => (
+          <React.Fragment key={el.id}>
+            <span>{`EMAIL: ${el.email}`}</span>
+            <p>{`BODY: ${el.body}`}</p>
+          </React.Fragment>
+        ))}
     </>
   );
 };
